ea8b445650d32dcaff690ad966857d23e380b5ae
MotM-MrRager <andy.gasper1@gmail.com>
2017-03-22 16:25:00 -0700

    Added functionality where dogs are appended to the DOM following getting them

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-console.log("Test console log to see if I linked the javascript file correctly");
-$(document).ready(function() {
-    $("button").on("click", function () {
-        console.log($(".animalType").text());
-    });
-});
+// console.log("Test console log to see if I linked the javascript file correctly");
+// $(document).ready(function(){
+// $("button").on("click",function(){
+//     console.log($(".animalType").text());
+// });
