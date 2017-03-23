f47f31a0ead673faa2e6fa96aba89310d35af1a5
MotM-MrRager <andy.gasper1@gmail.com>
2017-03-23 16:55:25 -0700

    quick save

--- petFinder.js
+++ petFinder.js
@@ -77,7 +77,7 @@ function displayPet(petObject) {
     if (petObject.length !== 0) {
         var petCarouselDiv = $("<div id= 'petCarousel' class = 'carousel slide'>");
         var innerPetCarousel = $("<div class= 'carousel-inner'>");
-        var dummyDiv = $("<div class = 'item active'>").text("BLAH!");
+        var dummyDiv = $("<div class = 'item active'>");
         petCarouselDiv.append(innerPetCarousel);
         $(innerPetCarousel).append(dummyDiv);
         $("#petInfo").append(petCarouselDiv);
